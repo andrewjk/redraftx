e27adbbd55431463c5a2f7d5f289b353ca64622e
Andrew K <andrewjk@users.noreply.github.com>
2025-08-27 14:56:17 +1000

    trying something

--- site.config.ts
+++ site.config.ts
@@ -5,6 +5,14 @@ import "dotenv/config";
 
 const site: Site = new Site();
 
+// HACK: To be able to import `.torp` files from barrel files in node_modules,
+// we need to add their libraries to `ssr.noExternal`
+site.viteConfig = {
+	ssr: {
+		noExternal: ["@torpor/ui"],
+	},
+};
+
 await defineSite(site, cloudflare, siteAdapter);
 
 export default site;
